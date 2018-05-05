1175d47ba29cb69963258e1423efd77d1c9e5436
Franky <liushuaigeq@163.com>
2018-05-06 00:15:30 +0800

    new version

--- client/assets/Script/HelloWorld.js
+++ client/assets/Script/HelloWorld.js
@@ -1,3 +1,4 @@
+
 cc.Class({
     extends: cc.Component,
 
@@ -13,6 +14,26 @@ cc.Class({
     // use this for initialization
     onLoad: function () {
         this.label.string = this.text;
+        pomelo.init({
+            host: "127.0.0.1",
+            port: 3101,
+            log: true
+        }, function () {
+            pomelo.request('gate.gateHandler.queryEntry', {}, function (msg) {
+                cc.log(msg);
+                pomelo.disconnect(function () {
+                    pomelo.init({
+                        host: msg.host,
+                        port: msg.port,
+                        reconnect: true
+                    }, function () {
+                        pomelo.request('connector.entryHandler.entry', {}, function (msg) {
+                            cc.log(msg);  
+                        });
+                    })
+                });
+            })
+        });
     },
 
     // called every frame
