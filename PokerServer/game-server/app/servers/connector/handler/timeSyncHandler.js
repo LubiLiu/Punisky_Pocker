f97c0a5eb239cd45ae2ed43eb721695f8b34d234
Franky <liushuaigeq@163.com>
2018-05-05 03:48:09 -0700

    del all

--- PokerServer/game-server/app/servers/connector/handler/timeSyncHandler.js
+++
@@ -1,8 +0,0 @@
-var moment = require('moment');
-
-var define = require('../../../../../shared/define');
-var handler = module.exports;
-
-handler.timeSync = function(msg, session, next) {
-  next(null, {code: define.OK,msg: moment().unix()});
-};
